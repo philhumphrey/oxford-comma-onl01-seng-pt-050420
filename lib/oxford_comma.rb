f2847802e9461944366516416e3a57a36f544d5b
Phil Humphrey <phumphrey588@yahoo.com>
2020-05-14 21:36:33 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -4,4 +4,13 @@ def oxford_comma(array)
    kiwis <<
  end
  
- 
+ def oxford_comma(array)
+    if array.length == 1
+      return array[0]
+    elsif array.length == 2
+      array.join(" and ")
+      elsif array.length >= 3
+      array[-1] = "and #{array[-1]}"
+      array.join(", ")
+    end
+  end
