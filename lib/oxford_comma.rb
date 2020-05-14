8cc0f860f964968279a295ab2f8bc683bda32a9f
Phil Humphrey <phumphrey588@yahoo.com>
2020-05-14 21:42:35 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,8 +1,8 @@
- def oxford_comma(array)
- array = ["beginnings"]
- first_array.join do |"beginnings"|
-   kiwis <<
- end
+# def oxford_comma(array)
+# array = ["beginnings"]
+# first_array.join do |"beginnings"|
+#   kiwis <<
+# end
  
  def oxford_comma(array)
     if array.length == 1
