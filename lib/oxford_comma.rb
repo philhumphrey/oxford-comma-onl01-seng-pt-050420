8f0130e8d87758ac3cc816de66b0381e42a4b53e
Phil Humphrey <phumphrey588@yahoo.com>
2020-05-14 22:03:38 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,18 @@
-def oxford_comma(array)
+# def oxford_comma(array)
+# array = ["beginnings"]
+# first_array.join do |"beginnings"|
+# 
 
-end
+
+# end
+ 
+ def oxford_comma(array)
+    if array.length == 1
+      return array[0]
+    elsif array.length == 2
+      array.join(" and ")
+      elsif array.length >= 3
+      array[-1] = "and #{array[-1]}"
+      array.join(", ")
+    end
+  end
